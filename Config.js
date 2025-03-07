a976833df819541feeff02d5bee40646c9844f05
Prince4great <princeemmanuel3040@gmail.com>
2025-03-07 02:39:51 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,16 +1,16 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "1234564789", // your number with country code
-  OWNER_NAME: 'シ', // your owner name 
-  BOT_NAME: 'Kord AI', // your preferred bot name
-  SESSION_ID: '', // replace your_sessionid with yours and make sure its inside the 
+  OWNER_NUMBERS: "2348062925069", // your number with country code
+  OWNER_NAME: Prince', // your owner name 
+  BOT_NAME: 'Smart', // your preferred bot name
+  SESSION_ID: 'kord_ai-gOJQ9ALfaipn', // replace your_sessionid with yours and make sure its inside the 
   INVINCIBLE_MODE: true, // toogle invincible mode 
-  WORK_MODE: 'public',
+  WORK_MODE: 'private',
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
-  AUTO_READ_STATUS: true,
+  AUTO_READ_STATUS: false,
   PREFIX: [".", "/", "!"],
   REJECT_CALLS: false,
   GROUP_EVENTS: false,
@@ -18,12 +18,12 @@ global.settings = {
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
   MAX_COMMANDS_PER_MINUTE: 100,
   MAX_DOWNLOAD_SIZE: 100,
-  AUTO_LIKE_STATUS: true,
+  AUTO_LIKE_STATUS: false,
   AUTO_LIKE_EMOJI: '✨',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "Opay", // the name of your preferred bank
+    acc_number: "8062925069", // account number for your preferred bank
+    acc_name: "Deborah Nnanna" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
@@ -42,6 +42,6 @@ global.settings = {
   ANTI_PM_SPAM_COUNT: 6,
   AUTO_TYPING: false, // if you wnat the bot to be typing always 
   AUTO_RECORDING: false, //if you want the bot to be recording always
-  ALWAYS_ONLINE: false,
+  ALWAYS_ONLINE: true,
 
 };
